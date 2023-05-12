e6bab9b16102bc9e4fc347854ae2653500f967be
Anton Kulagin <101942453+KulaginAG@users.noreply.github.com>
2023-05-12 06:16:45 +0300

    Исправлены комментарии

--- run_scripts.cmd
+++ run_scripts.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-REM Функция запускает набор скриптов и повторяет их выполнение при ошибке с последнего неудачного скрипта
+REM Функция запускает набор сценариев и повторяет их выполнение при ошибке полностью с самого начала
 REM Параметры
 REM %1 - директория, в которой будут выполняться сценарии
 REM %2 - время задержки перед повторной попыткой выполнения, в секундах
@@ -13,32 +13,18 @@ cd /d %1
 set RETRY_TIMEOUT=%2
 set SCRIPTS=%3 %4 %5 %6 %7 %8 %9
 :loop
-set ERROR_SCRIPT=
 for %%i in (%SCRIPTS%) do (
     echo Running script "%%i"...
     call %%i
     if errorlevel 1 (
-        echo Script "%%i" failed. Marking script for retry...
-        set ERROR_SCRIPT=%%i
-        goto :retry
+        echo Script "%%i" failed. Retrying in %RETRY_TIMEOUT% seconds...
+        timeout /T %RETRY_TIMEOUT% >nul
+        goto loop
     ) else (
         echo Script "%%i" completed successfully.
         timeout /T 5 >nul
     )
 )
 echo All scripts completed successfully.
-exit /b
-
-:retry
-echo Retrying script "%ERROR_SCRIPT%"...
-call %ERROR_SCRIPT%
-if errorlevel 1 (
-    echo Script "%ERROR_SCRIPT%" failed again. Retrying in %RETRY_TIMEOUT% seconds...
-    timeout /T %RETRY_TIMEOUT% >nul
-    goto :retry
-) else (
-    echo Script "%ERROR_SCRIPT%" completed successfully.
-    timeout /T 5 >nul
-    set ERROR_SCRIPT=
-    goto :loop
-)
+endlocal
+goto :eof
