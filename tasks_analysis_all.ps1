d5b1162c58d7e5be5240a4824d1c47bfe663701d
Anton Kulagin <101942453+KulaginAG@users.noreply.github.com>
2023-05-16 01:25:49 +0300

    Update tasks_analysis_all.ps1

--- tasks_analysis_all.ps1
+++ tasks_analysis_all.ps1
@@ -1,21 +1,27 @@
-$path = "FOLDER"
+$path = "TasksFolder"
+$today = Get-Date -Format 'yyyy-MM-dd'
+$outputFilePath = 'C:\PATH\TO\FILE.csv'
+
 Get-WinEvent -FilterHashtable @{
     'LogName' = 'Microsoft-Windows-TaskScheduler/Operational'
     'ID'      = 200, 201
 } | Where-Object {
-    $_.Properties[0].Value -like "*\$path\*"
-}| Group-Object ActivityID | ForEach-Object {
+    $_.Properties[0].Value -like "*\$path\*" -and
+    $_.TimeCreated.Date -eq $today
+} | Group-Object ActivityID | ForEach-Object {
     $start = $_.Group |
              Where-Object { $_.Id -eq 200 -and $_.Properties[0].Value -like "*\$path\*" } |
-             Select-Object -Expand TimeCreated -First 1
+             Select-Object -ExpandProperty TimeCreated -First 1
     $end   = $_.Group |
              Where-Object { $_.Id -eq 201 -and $_.Properties[0].Value -like "*\$path\*" } |
-             Select-Object -Expand TimeCreated -First 1
+             Select-Object -ExpandProperty TimeCreated -First 1
 
-    New-Object -Type PSObject -Property @{
+    $taskData = [PSCustomObject]@{
         'TaskName'  = $_.Group[0].Properties[0].Value
         'StartTime' = $start
-        'EndTime' = $end
+        'EndTime'   = $end
         'Duration'  = ($end - $start).TotalSeconds
-        }
     }
+
+    $taskData | Export-Csv -Path $outputFilePath -Append -NoTypeInformation
+}
