d08fd34ea6372a1a0d42c07f92b25a9d0b0b34eb
Anton Kulagin <101942453+KulaginAG@users.noreply.github.com>
2023-05-23 16:34:37 +0300

    Refactor botanic_dictionary.py

--- botanic_dictionary.py
+++ botanic_dictionary.py
@@ -2,41 +2,24 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-# Базовый URL-адрес веб-сайта
 base_url = "https://www.greeninfo.ru/alphabet.html/letter/"
-
-# Количество страниц
 num_pages = 28
 
-# Создаем список для хранения данных
-l = []
 
-# Проходим по каждой странице
-for page_num in range(num_pages):
-    # Создаем полный URL-адрес для каждой страницы
+def extract_names_from_page(page_num):
     url = base_url + str(page_num)
-    
-    # Отправляем GET-запрос по URL-адресу
     response = requests.get(url)
-    
-    # Анализируем HTML-содержимое
     soup = BeautifulSoup(response.content, "html.parser")
-    
-    # Находим все элементы с классом "em14"
     names = soup.find_all(class_="em14")
-    
-    # Извлекаем названия продуктов и записываем их в созданный список
-    for element in names:
-        name = element.text.strip()
-        if len(name) < 50: # Убираем нерелевантные значения
-            l.append(name.lower())
-    
-    # Отслеживаем выполнение
-    print("Страница", page_num, "просмотрена.")
+    return [name.text.strip().lower() for name in names if len(name.text.strip()) < 50]
+
+
+wordlist = []
+for page_num in range(num_pages):
+    wordlist += extract_names_from_page(page_num)
+    print(f"Страница {page_num} просмотрена")
 
 print("Извлечение данных завершено. Сохранено в переменной l")
 
-# Конвертируем список в датафрейм и экспортируем в csv
-df = pd.DataFrame(l, columns=['name'])
-df.head()
-df.to_csv('botanic_dictionary.csv', index=False, sep='\t', encoding='utf-16')
+df = pd.DataFrame(wordlist, columns=['name'])
+df.to_csv('botanic_dictionary.csv', index=False, sep='\t')
